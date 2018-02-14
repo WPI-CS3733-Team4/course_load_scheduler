7b2378592226776e30839e49765cb2d9304fe291
FabiGazi <fgaziano98@gmail.com>
2018-02-14 14:37:55 -0500

    Department

--- src/main/java/org/dselent/course_load_scheduler/client/model/Department.java
+++ src/main/java/org/dselent/course_load_scheduler/client/model/Department.java
@@ -1,4 +1,84 @@
 package org.dselent.course_load_scheduler.client.model;
 
-public class Department {
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Objects;
+
+public class Department extends Model {
+    // attributes
+
+    private Integer id;
+    private String department;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    // methods
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        if (createdAt != null) {
+            this.createdAt = createdAt.toInstant();
+        }
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        if (updatedAt != null) {
+            this.updatedAt = updatedAt.toInstant();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(department, that.department) &&
+                Objects.equals(createdAt, that.createdAt) &&
+                Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, department, createdAt, updatedAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", department='" + department + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
