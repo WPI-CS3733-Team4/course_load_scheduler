4849444e5258aa786d3657ba6dd8bbb8c89fa757
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-14 23:01:18 -0500

    completed courseAddAction

---
+++ src/main/java/org/dselent/course_load_scheduler/client/action/CourseAddAction.java
@@ -0,0 +1,35 @@
+package org.dselent.course_load_scheduler.client.action;
+
+public class CourseAddAction
+{
+    private String courseName;
+    private String courseDept;
+    private String courseDescription;
+
+    public CourseAddAction(String courseName, String courseDept, String courseDescription) {
+        this.courseName = courseName;
+        this.courseDept = courseDept;
+        this.courseDescription = courseDescription;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseDept() {
+        return courseDept;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseAddAction{" +
+                "courseName='" + courseName + '\'' +
+                ", courseDept='" + courseDept + '\'' +
+                ", courseDescription='" + courseDescription + '\'' +
+                '}';
+    }
+}
