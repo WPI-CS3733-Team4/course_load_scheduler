84b2cf81aa84aed97b0586530a4a9dc238407d97
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-15 01:56:31 -0500

    completed termModifyAction

---
+++ src/main/java/org/dselent/course_load_scheduler/client/action/TermModifyAction.java
@@ -0,0 +1,28 @@
+package org.dselent.course_load_scheduler.client.action;
+
+public class TermModifyAction
+{
+    private Integer termId;
+    private String termName;
+
+    public TermModifyAction(Integer termId, String termName) {
+        this.termId = termId;
+        this.termName = termName;
+    }
+
+    public Integer getTermId() {
+        return termId;
+    }
+
+    public String getTermName() {
+        return termName;
+    }
+
+    @Override
+    public String toString() {
+        return "TermModifyAction{" +
+                "termId=" + termId +
+                ", termName='" + termName + '\'' +
+                '}';
+    }
+}
