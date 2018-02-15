a5892d7eac75da253f235f96cdbfe5773570e075
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-15 01:22:59 -0500

    completed locationAddAction

---
+++ src/main/java/org/dselent/course_load_scheduler/client/action/LocationAddAction.java
@@ -0,0 +1,35 @@
+package org.dselent.course_load_scheduler.client.action;
+
+public class LocationAddAction
+{
+    private String building;
+    private Integer room;
+    private Integer roomSize;
+
+    public LocationAddAction(String building, Integer room, Integer roomSize) {
+        this.building = building;
+        this.room = room;
+        this.roomSize = roomSize;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public Integer getRoom() {
+        return room;
+    }
+
+    public Integer getRoomSize() {
+        return roomSize;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationAddAction{" +
+                "building='" + building + '\'' +
+                ", room=" + room +
+                ", roomSize=" + roomSize +
+                '}';
+    }
+}
