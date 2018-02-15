2b678f7ea9570e6072ba33e2060c779c6aef6469
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-15 01:50:35 -0500

    changed variable name

--- src/main/java/org/dselent/course_load_scheduler/client/action/LocationModifyAction.java
+++ src/main/java/org/dselent/course_load_scheduler/client/action/LocationModifyAction.java
@@ -2,20 +2,20 @@ package org.dselent.course_load_scheduler.client.action;
 
 public class LocationModifyAction
 {
-    private Integer location_id;
+    private Integer locationId;
     private String building;
     private Integer room;
     private Integer roomSize;
 
-    public LocationModifyAction(Integer location_id, String building, Integer room, Integer roomSize) {
-        this.location_id = location_id;
+    public LocationModifyAction(Integer locationId, String building, Integer room, Integer roomSize) {
+        this.locationId = locationId;
         this.building = building;
         this.room = room;
         this.roomSize = roomSize;
     }
 
-    public Integer getLocation_id() {
-        return location_id;
+    public Integer getlocationId() {
+        return locationId;
     }
 
     public String getBuilding() {
@@ -33,7 +33,7 @@ public class LocationModifyAction
     @Override
     public String toString() {
         return "LocationModifyAction{" +
-                "location_id=" + location_id +
+                "locationId=" + locationId +
                 ", building='" + building + '\'' +
                 ", room=" + room +
                 ", roomSize=" + roomSize +
