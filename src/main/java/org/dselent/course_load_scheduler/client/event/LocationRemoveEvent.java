f2925cfa71fd2a242c2edfde940a219e79f14270
NathanSiegel <siegelnathan08@gmail.com>
2018-02-16 00:07:49 -0500

    Finished Location Events and Handlers

--- src/main/java/org/dselent/course_load_scheduler/client/event/LocationRemoveEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/LocationRemoveEvent.java
@@ -1,5 +1,45 @@
 package org.dselent.course_load_scheduler.client.event;
 
-public class LocationRemoveEvent {
+import org.dselent.course_load_scheduler.client.action.LocationRemoveAction;
+import org.dselent.course_load_scheduler.client.event_handler.LocationRemoveEventHandler;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+
+/* Created by Nathan Siegel */
+
+public class LocationRemoveEvent extends GwtEvent<LocationRemoveEventHandler>{
+	
+	public static Type<LocationRemoveEventHandler> TYPE = new Type<LocationRemoveEventHandler>();
+	
+	private LocationRemoveAction action;
+	
+	public LocationRemoveEvent(LocationRemoveAction action)
+	{
+		this.action = action;
+	}
+	
+	public LocationRemoveAction getAction()
+	{
+		return action;
+	}
+	
+	/*
+	 * 
+	 */
+	@Override
+	public Type<LocationRemoveEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+	/*
+	 * 
+	 */
+	@Override
+	protected void dispatch(LocationRemoveEventHandler handler)
+	{
+		handler.onLocationRemove(this);
+	}
 
 }
