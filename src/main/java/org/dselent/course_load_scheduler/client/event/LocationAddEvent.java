1f5d546bef2adb761575e5479cf717decaed6c6c
NathanSiegel <siegelnathan08@gmail.com>
2018-02-15 23:57:14 -0500

    Some work on Location Events and Handlers. made template to make work on Events and Handlers 00% better

--- src/main/java/org/dselent/course_load_scheduler/client/event/LocationAddEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/LocationAddEvent.java
@@ -1,5 +1,45 @@
 package org.dselent.course_load_scheduler.client.event;
 
-public class LocationAddEvent {
+import org.dselent.course_load_scheduler.client.action.LocationAddAction;
+import org.dselent.course_load_scheduler.client.event_handler.LocationAddEventHandler;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+
+/* Created by Nathan Siegel */
+
+public class LocationAddEvent extends GwtEvent<LocationAddEventHandler>{
+	
+	public static Type<LocationAddEventHandler> TYPE = new Type<LocationAddEventHandler>();
+	
+	private LocationAddAction action;
+	
+	public LocationAddEvent(LocationAddAction action)
+	{
+		this.action = action;
+	}
+	
+	public LocationAddAction getAction()
+	{
+		return action;
+	}
+	
+	/*
+	 * 
+	 */
+	@Override
+	public Type<LocationAddEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+	/*
+	 * 
+	 */
+	@Override
+	protected void dispatch(LocationAddEventHandler handler)
+	{
+		handler.onLocationAdd(this);
+	}
 
 }
---
+++ src/main/java/org/dselent/course_load_scheduler/client/event_handler/TemplateEventHandler.java
@@ -0,0 +1,13 @@
+package org.dselent.course_load_scheduler.client.event_handler;
+
+import org.dselent.course_load_scheduler.client.event.$$Event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+/* Created by Nathan Siegel */
+
+public interface $$EventHandler extends EventHandler{
+	
+	public void on$$($$Event evt);
+
+}
