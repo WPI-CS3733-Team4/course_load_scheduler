9b8dd63d8684b318c1830b0998dd050416f8b2ea
Robaire <robaire@galliath.com>
2018-02-15 23:27:30 -0500

    Created SendLogoutEventHandler

---
+++ src/main/java/org/dselent/course_load_scheduler/client/event_handler/SendLogoutEventHandler.java
@@ -0,0 +1,6 @@
+package org.dselent.course_load_scheduler.client.event_handler;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface SendLogoutEventHandler extends EventHandler {
+}
