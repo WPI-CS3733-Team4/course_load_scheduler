4db22f0f8ff5f3b83caf93d24292953753e7b850
Krishna Madhurkar <krishnamadhurkar10@gmail.com>
2018-02-16 13:38:05 -0500

    Finished BuilderPresenter.java (Needs re-cheking)

--- src/main/java/org/dselent/course_load_scheduler/client/presenter/BuilderPresenter.java
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/BuilderPresenter.java
@@ -2,5 +2,26 @@ package org.dselent.course_load_scheduler.client.presenter;
 
 public interface BuilderPresenter extends BasePresenter{
 
-
+	IndexPresenter getParentPresenter();
+	void setParentPresenter(IndexPresenter parentPresenter);
+	void AddCourses();
+	void ModifyCourses();
+	void RemoveCourses();
+	void AddSections();
+	void ModifySections();
+	void RemoveSections();
+	void AddCoursSectionTimes();
+	void ModifyoursSectionTimes();
+	void RemoveoursSectionTimes();
+	void AddLocations();
+	void ModifyLocations();
+	void RemoveLocations();
+	void AddDepartments();
+	void ModifyDepartments();
+	void RemoveDepartments();
+	void AddTerms();
+	void ModifyTerms();
+	void RemoveTerms();
+	void ComfirmRequest();
+	
 }
