82ceed542afc52f405adab63a7d1043f524ccb5c
NathanSiegel <siegelnathan08@gmail.com>
2018-02-16 14:32:28 -0500

    Added InvalidRegisterStrings for error checking

---
+++ src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidRegisterStrings.java
@@ -0,0 +1,13 @@
+package org.dselent.course_load_scheduler.client.errorstring;
+
+/* Created by Nathan Siegel */
+
+public class InvalidRegisterStrings
+{
+	public static final String NULL_USER_NAME = "Your user name cannot be empty.";
+	public static final String NULL_FIRST_NAME = "Your first name cannot be empty.";
+	public static final String NULL_LAST_NAME = "Your last name cannot be empty.";
+	public static final String NULL_EMAIL = "Your email cannot be empty.";
+	public static final String NULL_PASSWORD = "Your password cannot be empty.";
+
+}
