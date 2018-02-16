e0de40074da6dc0327178bfdf1115447cefa6124
Krishna Madhurkar <krishnamadhurkar10@gmail.com>
2018-02-16 13:29:48 -0500

    finished AdminPresenter.java (someone needs to re-check)

--- src/main/java/org/dselent/course_load_scheduler/client/presenter/AdminPresenter.java
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/AdminPresenter.java
@@ -1,5 +1,19 @@
 package org.dselent.course_load_scheduler.client.presenter;
 
-public interface AdminPresenter {
+public interface AdminPresenter extends BasePresenter {
 
+	IndexPresenter getParentPresenter();
+	void setParentPresenter(IndexPresenter parentPresenter);
+	void changeUserRoles();
+	void confirmUser();
+	void AddUsers();
+	void ModifyUsers();
+	void RemoveUsers();
+	void AddRoles();
+	void ModifyRoles();
+	void RemoveRoles();
 }
+
+
+
+
