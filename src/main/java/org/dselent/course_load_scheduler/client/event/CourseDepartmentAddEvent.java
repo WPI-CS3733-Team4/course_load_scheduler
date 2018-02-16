f34f2e09736f3efbdd7f93a27fc96f38d63f7f7e
NathanSiegel <siegelnathan08@gmail.com>
2018-02-16 01:43:03 -0500

    completed CourseDepartmentAddEvent because it hadn't been

--- src/main/java/org/dselent/course_load_scheduler/client/event/CourseDepartmentAddEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/CourseDepartmentAddEvent.java
@@ -1,4 +1,45 @@
 package org.dselent.course_load_scheduler.client.event;
 
-public class CourseDepartmentAddEvent {
+import org.dselent.course_load_scheduler.client.action.CourseDepartmentAddAction;
+import org.dselent.course_load_scheduler.client.event_handler.CourseDepartmentAddEventHandler;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+
+/* Created by Nathan Siegel */
+
+public class CourseDepartmentAddEvent extends GwtEvent<CourseDepartmentAddEventHandler>{
+	
+	public static Type<CourseDepartmentAddEventHandler> TYPE = new Type<CourseDepartmentAddEventHandler>();
+	
+	private CourseDepartmentAddAction action;
+	
+	public CourseDepartmentAddEvent(CourseDepartmentAddAction action)
+	{
+		this.action = action;
+	}
+	
+	public CourseDepartmentAddAction getAction()
+	{
+		return action;
+	}
+	
+	/*
+	 * 
+	 */
+	@Override
+	public Type<CourseDepartmentAddEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+	/*
+	 * 
+	 */
+	@Override
+	protected void dispatch(CourseDepartmentAddEventHandler handler)
+	{
+		handler.onCourseDepartmentAdd(this);
+	}
+
 }
