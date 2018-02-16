b97743387f1570bb33f69d803be1ababff61fda3
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-16 17:04:39 -0500

    work on builderview

--- src/main/java/org/dselent/course_load_scheduler/client/view/BuilderView.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/BuilderView.java
@@ -1,5 +1,24 @@
 package org.dselent.course_load_scheduler.client.view;
 
+import org.dselent.course_load_scheduler.client.presenter.LoginPresenter;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.TextBox;
+
 public interface BuilderView {
+	TextBox getCourseTextBox();
+	void setCourseTextBox(TextBox courseTextBox);
+	TextBox getSectionTextBox();
+	void setSectionTextBox(TextBox sectionTextBox);
+	TextBox getTimeTextBox();
+	void setTimeTextBox(TextBox timeTextBox);
+	TextBox getLocationTextBox();
+	void setLocationTextBox(TextBox locationTextBox);
+	TextBox getDepartmentTextBox();
+	void setDepartmentTextBox(TextBox departmentTextBox);
+	TextBox getTermTextBox();
+	void setTermTextBox(TextBox termTextBox);
+	Button getEnterButton();
+	void setEnterButton(Button enterButton);
+	
 
 }
