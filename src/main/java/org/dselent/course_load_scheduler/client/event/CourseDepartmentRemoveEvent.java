27b7c89892905b69ce7351db6389a1f1cef97ce7
NathanSiegel <siegelnathan08@gmail.com>
2018-02-16 00:46:27 -0500

    Course Department Modify and Remove Events/Handlers added

---
+++ src/main/java/org/dselent/course_load_scheduler/client/event/CourseDepartmentRemoveEvent.java
@@ -0,0 +1,45 @@
+package org.dselent.course_load_scheduler.client.event;
+
+import org.dselent.course_load_scheduler.client.action.CourseDepartmentRemoveAction;
+import org.dselent.course_load_scheduler.client.event_handler.CourseDepartmentRemoveEventHandler;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+
+/* Created by Nathan Siegel */
+
+public class CourseDepartmentRemoveEvent extends GwtEvent<CourseDepartmentRemoveEventHandler>{
+	
+	public static Type<CourseDepartmentRemoveEventHandler> TYPE = new Type<CourseDepartmentRemoveEventHandler>();
+	
+	private CourseDepartmentRemoveAction action;
+	
+	public CourseDepartmentRemoveEvent(CourseDepartmentRemoveAction action)
+	{
+		this.action = action;
+	}
+	
+	public CourseDepartmentRemoveAction getAction()
+	{
+		return action;
+	}
+	
+	/*
+	 * 
+	 */
+	@Override
+	public Type<CourseDepartmentRemoveEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+	/*
+	 * 
+	 */
+	@Override
+	protected void dispatch(CourseDepartmentRemoveEventHandler handler)
+	{
+		handler.onCourseDepartmentRemove(this);
+	}
+
+}
