b3f4539c96fbcbcc9115513ba7c21676db83db34
NathanSiegel <siegelnathan08@gmail.com>
2018-02-17 13:54:45 -0500

    Added error strings for invalid course requests

---
+++ src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidRequestStrings.java
@@ -0,0 +1,10 @@
+package org.dselent.course_load_scheduler.client.errorstring;
+
+/* Created by Nathan Siegel */
+
+public class InvalidRequestStrings {
+	public static final String INVALID_REQUEST_ID = "Your request ID must be a valid number.";
+	public static final String INVALID_FACULTY_ID = "Your faculty ID must be a valid number.";
+	public static final String INVALID_COURSE_SECTION_ID = "Your course section ID must be a valid number.";
+
+}
