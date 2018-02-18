c24275714868388f623c4c6241c3ecb9b5b99256
Michael Capobianco <mccapobianco@wpi.edu>
2018-02-17 19:42:19 -0500

    added buttons

--- src/main/java/org/dselent/course_load_scheduler/client/view/BuilderView.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/BuilderView.java
@@ -4,7 +4,7 @@ import org.dselent.course_load_scheduler.client.presenter.BuilderPresenter;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.TextBox;
 
-public interface BuilderView extends BaseView<BuilderPresenter>{
+	public interface BuilderView extends BaseView<BuilderPresenter>{
 	TextBox getCourseIdTextBox();
 	void setCourseIdTextBox(TextBox courseIdTextBox);
 	TextBox getCourseNameTextBox();
@@ -14,11 +14,11 @@ public interface BuilderView extends BaseView<BuilderPresenter>{
 	TextBox getCourseDescriptionTextBox();
 	void setCourseDescriptionTextBox(TextBox courseDescriptionTextBox);
 	TextBox getCourseSectionIdTextBox();
-	void setCourseSectionIdTextBox(TextBox coureSectionTextBox);
+	void setCourseSectionIdTextBox(TextBox courseSectionTextBox);
+	TextBox getCourseSectionTimeIdTextBox();
+	void setCourseSectionTimeIdTextBox(TextBox courseSectionTimeId);
 	TextBox getSectionTypeTextBox();
-	void getCourseSectionTimeIdTextBox();
-	TextBox setCourseSectionTimeIdTextBox(TextBox courseSectionTimeid);
-	void setSectionTypeTextBox(TextBox sectionTypeTextox);
+	void setSectionTypeTextBox(TextBox sectionTypeTextBox);
 	TextBox getStartTimeTextBox();
 	void setStartTimeTextBox(TextBox startTimeTextBox);
 	TextBox getEndTimeTextBox();
@@ -29,8 +29,8 @@ public interface BuilderView extends BaseView<BuilderPresenter>{
 	void setLocationTextBox(TextBox locationTextBox);
 	TextBox getLocationIdTextBox();
 	void setLocationIdTextBox(TextBox locationIdTextBox);
-	TextBox setBuildingTextBox();
-	void getBuildingTextBox(TextBox buildingTextBox);
+	TextBox getBuildingTextBox();
+	void setBuildingTextBox(TextBox buildingTextBox);
 	TextBox getRoomTextBox();
 	void setRoomTextBox(TextBox roomTextBox);
 	TextBox getRoomSizeTextBox();
@@ -38,13 +38,49 @@ public interface BuilderView extends BaseView<BuilderPresenter>{
 	TextBox getDepartmentTextBox();
 	void setDepartmentTextBox(TextBox departmentTextBox);
 	TextBox getDepartmentIdTextBox();
-	void setDepartmentIdTextbox(TextBox departmentIdTextBox);
+	void setDepartmentIdTextBox(TextBox departmentIdTextBox);
 	TextBox getTermIdTextBox();
 	void setTermIdTextBox(TextBox termIdTextBox);
 	TextBox getTermNameTextBox();
 	void setTermNameTextBox(TextBox termNameTextBox);
-	Button getEnterButton();
-	void setEnterButton(Button enterButton);
+	Button getAddCoursesButton();
+	void setAddCoursesButton(Button addCoursesButton);
+	Button getModifyCoursesButton();
+	void setModifyCoursesButton(Button modifyCoursesButton);
+	Button getRemoveCoursesButton();
+	void setRemoveCoursesButton(Button removeCoursesButton);
+	Button getAddSectionsButton();
+	void setAddSectionsButton(Button addSectionsButton);
+	Button getModifySectionsButton();
+	void setModifySectionsButton(Button modifySectionsButton);
+	Button getRemoveSectionsButton();
+	void setRemoveSectionsButton(Button removeSectionsButton);
+	Button getAddCourseSectionTimesButton();
+	void setAddCourseSectionTimesButton(Button addCourseSectionTimesButton);
+	Button getModifyCourseSectionTimesButton();
+	void setModifyCourseSectionTimesButton(Button modifyCourseSectionTimesButton);
+	Button getRemoveCourseSectionTimesButton();
+	void setRemoveCourseSectionTimesButton(Button removeCourseSectionTimesButton);
+	Button getAddLocationsButton();
+	void setAddLocationsButton(Button addLocationsButton);
+	Button getModifyLocationsButton();
+	void setModifyLocationsButton(Button modifyLocationsButton);
+	Button getRemoveLocationsButton();
+	void setRemoveLocationsButton(Button removeLocationsButton);
+	Button getAddDepartmentsButton();
+	void setAddDepartmentsButton(Button addDepartmentsButton);
+	Button getModifyDepartmentsButton();
+	void setModifyDepartmentsButton(Button modifyDepartmentsButton);
+	Button getRemoveDepartmentsButton();
+	void setRemoveDepartmentsButton(Button removeDepartmentsButton);
+	Button getAddTermsButton();
+	void setAddTermsButton(Button addTermsButton);
+	Button getModifyTermsButton();
+	void setModifyTermsButton(Button modifyTermsButton);
+	Button getRemoveTermsButton();
+	void setRemoveTermsButton(Button removeTermsButton);
+	Button getComfirmRequestButton();
+	void setComfirmRequestButton(Button comfirmRequestButton);
 	
 
 }
