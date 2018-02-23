166dfe8eaffc36da1a5a4a5c635a21ee4cebb2a9
Robaire <robaire@galliath.com>
2018-02-23 14:16:39 -0500

    Changed constructor to take a User

--- src/main/java/org/dselent/course_load_scheduler/client/action/ReceiveLoginAction.java
+++ src/main/java/org/dselent/course_load_scheduler/client/action/ReceiveLoginAction.java
@@ -1,15 +1,22 @@
 package org.dselent.course_load_scheduler.client.action;
 
+import org.dselent.course_load_scheduler.client.model.User;
+
 public class ReceiveLoginAction extends Action{
 
+	private User user;
 	private String userName;
 	private String password;
 	
-	public ReceiveLoginAction(String userName, String password) {
-		this.userName = userName;
-		this.password = password;
+	public ReceiveLoginAction(User user) {
+		this.user = user;
+		this.userName = user.getUserName();
+		this.password = user.getEncryptedPassword();
 	}
 	
+	public User getUser() {
+		return this.user;
+	}
 	public String getUserName() {
 		return this.userName;
 	}
