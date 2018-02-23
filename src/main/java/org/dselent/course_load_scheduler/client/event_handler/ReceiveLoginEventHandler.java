520ebd0033630b01bbf479b08cd0af1082bcfd06
Robaire <robaire@galliath.com>
2018-02-23 14:01:40 -0500

    Nothing

--- src/main/java/org/dselent/course_load_scheduler/client/event_handler/ReceiveLoginEventHandler.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event_handler/ReceiveLoginEventHandler.java
@@ -1,6 +1,6 @@
 package org.dselent.course_load_scheduler.client.event_handler;
-import org.dselent.course_load_scheduler.client.event.ReceiveLoginEvent;
 
+import org.dselent.course_load_scheduler.client.event.ReceiveLoginEvent;
 import com.google.gwt.event.shared.EventHandler;
 
 public interface ReceiveLoginEventHandler extends EventHandler
