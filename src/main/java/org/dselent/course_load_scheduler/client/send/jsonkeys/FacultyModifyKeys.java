fbff0295bacdd51b27f5c4e358967990e8c2cf9c
Krishna Madhurkar <krishnamadhurkar10@gmail.com>
2018-02-24 17:04:42 -0500

    Fixed merge error on FacultyKeys

--- src/main/java/org/dselent/course_load_scheduler/client/send/jsonkeys/FacultyModifyKeys.java
+++ src/main/java/org/dselent/course_load_scheduler/client/send/jsonkeys/FacultyModifyKeys.java
@@ -1,17 +1,9 @@
 package org.dselent.course_load_scheduler.client.send.jsonkeys;
 
-<<<<<<< HEAD
+
 public enum FacultyModifyKeys {
 	FACULTY_ID,
 	USER_ID,
 	REQUIRED_CREDITS;
 
 }
-=======
-public enum FacultyModifyKeys
-{
-	FACULTY_ID,
-	USER_ID,
-	REQUIRED_CREDITS;
-}
->>>>>>> ec4e075b7539bc2012995092d88cc40b1a2bbe08
--- src/main/java/org/dselent/course_load_scheduler/client/send/jsonkeys/FacultyRemoveKeys.java
+++ src/main/java/org/dselent/course_load_scheduler/client/send/jsonkeys/FacultyRemoveKeys.java
@@ -1,12 +1,6 @@
 package org.dselent.course_load_scheduler.client.send.jsonkeys;
 
-<<<<<<< HEAD
+
 public enum FacultyRemoveKeys {
 FACULTY_ID;
 }
-=======
-public enum FacultyRemoveKeys
-{
-	FACULTY_ID;
-}
->>>>>>> ec4e075b7539bc2012995092d88cc40b1a2bbe08
