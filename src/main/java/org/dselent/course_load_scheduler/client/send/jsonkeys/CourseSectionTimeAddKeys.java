3cdbec822de509efacdb5c342eefab864c4064ef
FabiGazi <fgaziano98@gmail.com>
2018-02-24 17:08:15 -0500

    CourseSectionTimeKeys.java

---
+++ src/main/java/org/dselent/course_load_scheduler/client/send/jsonkeys/CourseSectionTimeAddKeys.java
@@ -0,0 +1,10 @@
+package org.dselent.course_load_scheduler.client.send.jsonkeys;
+
+public enum CourseSectionTimeAddKeys
+{
+	COURSE_SECTION_ID,
+	DAY_OF_WEEK,
+	START_TIME,
+	END_TIME,
+	LOCATION;
+}
