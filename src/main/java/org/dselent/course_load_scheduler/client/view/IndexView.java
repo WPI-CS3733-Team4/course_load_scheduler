fe96dbd648d36dbed27daf2b06a2a37591301032
Robaire <robaire@galliath.com>
2018-02-24 16:50:51 -0500

    Removed Button methods

--- src/main/java/org/dselent/course_load_scheduler/client/view/IndexView.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/IndexView.java
@@ -2,7 +2,6 @@ package org.dselent.course_load_scheduler.client.view;
 
 import org.dselent.course_load_scheduler.client.presenter.IndexPresenter;
 
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.PopupPanel;
@@ -12,9 +11,5 @@ public interface IndexView extends BaseView<IndexPresenter>
 	HTMLPanel getMainPanel();
 	void setMainPanel(HTMLPanel mainPanel);
 	PopupPanel getGlassLoadingPanel();
-	Image getLoadingImage();
-	Button getFacultyButton();
-	Button getBuilderButton();
-	Button getAdminButton();
-	
+	Image getLoadingImage();	
 }
