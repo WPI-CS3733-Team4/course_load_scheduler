2df4736742a19d3bd35557a8bd487edcb5ac2901
NathanSiegel <siegelnathan08@gmail.com>
2018-02-24 18:16:39 -0500

    Term Add,Modify,Remove events include method that had been forgotten

--- src/main/java/org/dselent/course_load_scheduler/client/event/TermAddEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/TermAddEvent.java
@@ -4,6 +4,7 @@ import org.dselent.course_load_scheduler.client.action.TermAddAction;
 import org.dselent.course_load_scheduler.client.event_handler.TermAddEventHandler;
 
 import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.ui.HasWidgets;
 
 
 /* Created by Nathan Siegel */
@@ -40,6 +41,10 @@ public class TermAddEvent extends GwtEvent<TermAddEventHandler>{
 	protected void dispatch(TermAddEventHandler handler)
 	{
 		handler.onTermAdd(this);
+	}
+	
+	public HasWidgets getContainer() {
+		return this.getContainer();
 	}
 
 }
--- src/main/java/org/dselent/course_load_scheduler/client/event/TermRemoveEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/TermRemoveEvent.java
@@ -4,6 +4,7 @@ import org.dselent.course_load_scheduler.client.action.TermRemoveAction;
 import org.dselent.course_load_scheduler.client.event_handler.TermRemoveEventHandler;
 
 import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.ui.HasWidgets;
 
 
 /* Created by Nathan Siegel */
@@ -40,6 +41,10 @@ public class TermRemoveEvent extends GwtEvent<TermRemoveEventHandler>{
 	protected void dispatch(TermRemoveEventHandler handler)
 	{
 		handler.onTermRemove(this);
+	}
+	
+	public HasWidgets getContainer() {
+		return this.getContainer();
 	}
 
 }
