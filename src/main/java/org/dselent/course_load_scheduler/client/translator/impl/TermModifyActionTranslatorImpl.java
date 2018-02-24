2eba1c93a9d942c341860f05c4f56dd2593ecb2b
teejay <thananartpiya@gmail.com>
2018-02-24 17:54:25 -0500

    Completed TermRemoveActionImpl

--- src/main/java/org/dselent/course_load_scheduler/client/translator/impl/TermModifyActionTranslatorImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/translator/impl/TermModifyActionTranslatorImpl.java
@@ -1,5 +1,35 @@
 package org.dselent.course_load_scheduler.client.translator.impl;
 
-public class TermModifyActionTranslatorImpl {
+import org.dselent.course_load_scheduler.client.action.TermModifyAction;
+import org.dselent.course_load_scheduler.client.send.jsonkeys.TermModifyKeys;
+import org.dselent.course_load_scheduler.client.translator.ActionTranslator;
+import org.dselent.course_load_scheduler.client.utils.JSONHelper;
 
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONValue;
+
+public class TermModifyActionTranslatorImpl implements ActionTranslator<TermModifyAction, TermModifyAction>{
+
+	@Override
+	public JSONObject translateToJson(TermModifyAction action) {
+		JSONObject jsonObject = new JSONObject();
+		
+		JSONHelper.putIntValue(jsonObject, JSONHelper.convertKeyName(TermModifyKeys.TERM_ID), action.getTermId());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(TermModifyKeys.TERM_NAME), action.getTermName());
+	
+		return jsonObject;
+	}
+	
+	@Override
+	public TermModifyAction translateToAction(JSONObject json) {
+		JSONValue jsonObject = json.get("success");
+		JSONObject userObject = jsonObject.isArray().get(0).isObject();
+		
+		String termId = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(TermModifyKeys.TERM_ID));
+		String termName = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(TermModifyKeys.TERM_NAME));
+	
+		TermModifyAction action = new TermModifyAction(termId,termName);	
+		
+		return action;
+	}
 }
