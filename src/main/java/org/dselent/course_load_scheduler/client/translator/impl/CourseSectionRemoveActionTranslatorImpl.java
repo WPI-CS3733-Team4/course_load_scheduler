1a2781e684e5f1f185a3f4616713ec4cd064843c
Krishna Madhurkar <krishnamadhurkar10@gmail.com>
2018-02-24 17:52:46 -0500

    Finished /CourseSectionRemoveActionTranslatorImpl.java

--- src/main/java/org/dselent/course_load_scheduler/client/translator/impl/CourseSectionRemoveActionTranslatorImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/translator/impl/CourseSectionRemoveActionTranslatorImpl.java
@@ -2,8 +2,8 @@ package org.dselent.course_load_scheduler.client.translator.impl;
 
 import org.dselent.course_load_scheduler.client.action.ReceiveLoginAction;
 import org.dselent.course_load_scheduler.client.receive.jsonkeys.ReceiveLoginKeys;
-import org.dselent.course_load_scheduler.client.action.CourseSectionModifyAction;
-import org.dselent.course_load_scheduler.client.send.jsonkeys.CourseSectionModifyKeys;
+import org.dselent.course_load_scheduler.client.action.CourseSectionRemoveAction;
+import org.dselent.course_load_scheduler.client.send.jsonkeys.CourseSectionRemoveKeys;
 import org.dselent.course_load_scheduler.client.translator.ActionTranslator;
 import org.dselent.course_load_scheduler.client.utils.JSONHelper;
 
@@ -12,24 +12,22 @@ import com.google.gwt.json.client.JSONValue;
 
 /* Created by Krishna Madhurkar*/
 
-public class CourseSectionRemoveActionTranslatorImpl implements ActionTranslator<CourseSectionModifyAction, CourseSectionModifyAction>
+public class CourseSectionRemoveActionTranslatorImpl implements ActionTranslator<CourseSectionRemoveAction, CourseSectionRemoveAction>
 {
 	@Override
-	public JSONObject translateToJson(CourseSectionModifyAction action)
+	public JSONObject translateToJson(CourseSectionRemoveAction action)
 	{
 		JSONObject jsonObject = new JSONObject();
 		
-		JSONHelper.putIntValue(jsonObject, JSONHelper.convertKeyName(CourseSectionModifyKeys.COURSE_SECTION_ID), action.getCourseSectionId());
-		JSONHelper.putIntValue(jsonObject, JSONHelper.convertKeyName(CourseSectionModifyKeys.COURSE_ID), action.getCourseId());
-		JSONHelper.putIntValue(jsonObject, JSONHelper.convertKeyName(CourseSectionModifyKeys.SECTION_TYPE), action.getSectionType());
-		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(CourseSectionModifyKeys.TERM), action.getTerm());
+		JSONHelper.putIntValue(jsonObject, JSONHelper.convertKeyName(CourseSectionRemoveKeys.COURSE_SECTION_ID), action.getCourseSectionId());
+	
 		
 		
 		return jsonObject;
 	}
 	
 	@Override
-	public CourseSectionModifyAction translateToAction(JSONObject json)
+	public CourseSectionRemoveAction translateToAction(JSONObject json)
 	{		
 		// null values will not have their keys sent back from the sever
 		// this will throw an exception here
@@ -41,16 +39,11 @@ public class CourseSectionRemoveActionTranslatorImpl implements ActionTranslator
 		JSONObject userObject = jsonObject.isArray().get(0).isObject();
 		
 
-		Integer courseSectionId = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(CourseSectionModifyKeys.COURSE_SECTION_ID));
-		Integer courseId = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(CourseSectionModifyKeys.COURSE_ID));
-		Integer sectionType = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(CourseSectionModifyKeys.SECTION_TYPE));
-		String requiredCredits = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(CourseSectionModifyKeys.TERM));
-	
+		Integer courseSectionId = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(CourseSectionRemoveKeys.COURSE_SECTION_ID));
 		
 		
 		// possibly use builder pattern if it is a lot of data
-		CourseSectionModifyAction action = new CourseSectionModifyAction(courseSectionId, courseId, sectionType,requiredCredits);	
-	
+		CourseSectionRemoveAction action = new CourseSectionRemoveAction(courseSectionId);
 		return action;
 	}
 
