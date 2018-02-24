93ab9352689aaca36550aeabd4543ffd7e98cb23
NathanSiegel <siegelnathan08@gmail.com>
2018-02-24 15:35:37 -0500

    network request strings all added for POST requests

--- src/main/java/org/dselent/course_load_scheduler/client/network/NetworkRequestStrings.java
+++ src/main/java/org/dselent/course_load_scheduler/client/network/NetworkRequestStrings.java
@@ -4,7 +4,50 @@ public final class NetworkRequestStrings
 {
 	public static final String SERVER_LOCATION = "http://localhost:8080/";
 	public static final String BASE_REQUEST = "course_load_scheduler/";
+	
 	public static String LOGIN = "user/login";
+	public static String USER_ADD = "user/register";
+	public static String USER_MODIFY = "user/modify";
+	public static String USER_REMOVE = "user/deactivate";
+	public static String LOGOUT = "user/logout";
+	
+	public static String USER_PERMISSION_MODIFY = "admin/changeUserRole";
+	
+	public static String TERM_ADD = "term/add";
+	public static String TERM_MODIFY = "term/modify";
+	public static String TERM_REMOVE = "term/remove";
+	
+	public static String LOCATION_ADD = "location/add";
+	public static String LOCATION_MODIFY = "location/modify";
+	public static String LOCATION_REMOVE = "location/remove";
+	
+	public static String FACULTY_ADD = "faculty/add";
+	public static String FACULTY_MODIFY = "faculty/modify";
+	public static String FACULTY_REMOVE = "faculty/remove";
+	
+	public static String DEPARTMENT_ADD = "department/add";
+	public static String DEPARTMENT_MODIFY = "department/modify";
+	public static String DEPARTMENT_REMOVE = "department/remove";
+	
+	public static String COURSE_SECTION_TIMES_ADD = "courseSectionTime/add";
+	public static String COURSE_SECTION_TIMES_MODIFY = "courseSectionTime/modify";
+	public static String COURSE_SECTION_TIMES_REMOVE = "courseSectionTime/remove";
+	
+	public static String COURSE_SECTION_ADD = "courseSection/add";
+	public static String COURSE_SECTION_MODIFY = "courseSection/modify";
+	public static String COURSE_SECTION_REMOVE = "courseSection/remove";
+	
+	public static String COURSE_ADD = "course/add";
+	public static String COURSE_MODIFY = "course/modify";
+	public static String COURSE_REMOVE = "course/remove";
+	
+	public static String COURSE_REQUEST_ADD = "courseRequests/add";
+	public static String COURSE_REQUEST_MODIFY = "courseRequests/modify";
+	public static String COURSE_REQUEST_REMOVE = "courseRequests/remove";
+	
+	public static String COURSE_DEPARTMENT_ADD = "courseDepartments/add";
+	public static String COURSE_DEPARTMENT_REMOVE = "courseDepartments/modify";
+	public static String COURSE_DEPARTMENT_MODIFY = "courseDepartments/remove";
 	
 	private NetworkRequestStrings() {};
 }
