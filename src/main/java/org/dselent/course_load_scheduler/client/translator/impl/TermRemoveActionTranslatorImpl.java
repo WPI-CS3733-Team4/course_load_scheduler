60b6a135dd782f4e0ecfb35446ee25a4a383a8d6
teejay <thananartpiya@gmail.com>
2018-02-24 18:06:37 -0500

    Fixed bugs

--- src/main/java/org/dselent/course_load_scheduler/client/translator/impl/TermRemoveActionTranslatorImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/translator/impl/TermRemoveActionTranslatorImpl.java
@@ -1,5 +1,7 @@
 package org.dselent.course_load_scheduler.client.translator.impl;
 
+import org.dselent.course_load_scheduler.client.action.ReceiveLoginAction;
+import org.dselent.course_load_scheduler.client.receive.jsonkeys.ReceiveLoginKeys;
 import org.dselent.course_load_scheduler.client.action.TermRemoveAction;
 import org.dselent.course_load_scheduler.client.send.jsonkeys.TermRemoveKeys;
 import org.dselent.course_load_scheduler.client.translator.ActionTranslator;
@@ -7,6 +9,7 @@ import org.dselent.course_load_scheduler.client.utils.JSONHelper;
 
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONValue;
+
 
 public class TermRemoveActionTranslatorImpl implements ActionTranslator<TermRemoveAction,TermRemoveAction>{
 	
@@ -33,7 +36,7 @@ public class TermRemoveActionTranslatorImpl implements ActionTranslator<TermRemo
 		JSONValue jsonObject = json.get("success");
 		JSONObject userObject = jsonObject.isArray().get(0).isObject();
 		
-		String termId = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(TermRemoveKeys.TERM_ID));
+		Integer termId = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(TermRemoveKeys.TERM_ID));
 		
 		
 		// possibly use builder pattern if it is a lot of data
