e2003c4c3d54c061eea261706b84dc60b68318d0
NathanSiegel <siegelnathan08@gmail.com>
2018-02-25 18:59:02 -0500

    Location events modified to remove stack overflow error

--- src/main/java/org/dselent/course_load_scheduler/client/event/LocationAddEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/LocationAddEvent.java
@@ -1,6 +1,7 @@
 package org.dselent.course_load_scheduler.client.event;
 
 import org.dselent.course_load_scheduler.client.action.LocationAddAction;
+import org.dselent.course_load_scheduler.client.action.UserAddAction;
 import org.dselent.course_load_scheduler.client.event_handler.LocationAddEventHandler;
 
 import com.google.gwt.event.shared.GwtEvent;
@@ -9,15 +10,15 @@ import com.google.gwt.user.client.ui.HasWidgets;
 
 /* Created by Nathan Siegel */
 
-public class LocationAddEvent extends GwtEvent<LocationAddEventHandler>{
+public class LocationAddEvent extends DisplayEvent<LocationAddAction,LocationAddEventHandler>{
 	
 	public static Type<LocationAddEventHandler> TYPE = new Type<LocationAddEventHandler>();
 	
 	private LocationAddAction action;
 	
-	public LocationAddEvent(LocationAddAction action)
+	public LocationAddEvent(LocationAddAction action, HasWidgets container)
 	{
-		this.action = action;
+		super(action, container);
 	}
 	
 	public LocationAddAction getAction()
@@ -41,10 +42,6 @@ public class LocationAddEvent extends GwtEvent<LocationAddEventHandler>{
 	protected void dispatch(LocationAddEventHandler handler)
 	{
 		handler.onLocationAdd(this);
-	}
-	
-	public HasWidgets getContainer() {
-		return this.getContainer();
 	}
 
 }
