04b353900e83657ae9d70b8806953ae6195b3d31
teejay <thananartpiya@gmail.com>
2018-02-25 19:15:20 -0500

    requestevent done updated

--- src/main/java/org/dselent/course_load_scheduler/client/event/RequestEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/RequestEvent.java
@@ -2,22 +2,20 @@ package org.dselent.course_load_scheduler.client.event;
 
 import org.dselent.course_load_scheduler.client.action.RequestAction;
 import org.dselent.course_load_scheduler.client.event_handler.RequestEventHandler;
-
-import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.user.client.ui.HasWidgets;
 
 
 /* Created by Nathan Siegel */
 
-public class RequestEvent extends GwtEvent<RequestEventHandler>{
+public class RequestEvent extends DisplayEvent<RequestAction,RequestEventHandler>{
 	
 	public static Type<RequestEventHandler> TYPE = new Type<RequestEventHandler>();
 	
 	private RequestAction action;
 	
-	public RequestEvent(RequestAction action)
+	public RequestEvent(RequestAction action, HasWidgets container)
 	{
-		this.action = action;
+		super(action,container);
 	}
 	
 	public RequestAction getAction()
@@ -41,10 +39,6 @@ public class RequestEvent extends GwtEvent<RequestEventHandler>{
 	protected void dispatch(RequestEventHandler handler)
 	{
 		handler.onRequest(this);
-	}
-	
-	public HasWidgets getContainer() {
-		return this.getContainer();
 	}
 
 }
