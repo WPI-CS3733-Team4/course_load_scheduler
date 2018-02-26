a90d0e86ff9ba38736b25e2472d88db2d34fad5f
teejay <thananartpiya@gmail.com>
2018-02-26 01:46:40 -0500

    starteddepartment event

---
+++ src/main/java/org/dselent/course_load_scheduler/client/event/SendDepartmentEvent.java
@@ -0,0 +1,33 @@
+package org.dselent.course_load_scheduler.client.event;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+/* Created by Thananart Piyajarawong*/
+
+public class SendDepartmentEvent extends DisplayEvent<SendDepartmentAction,SendDepartmentEventHandler>
+	public static Type<SendDepartmentEventHandler> TYPE = new Type<SendDepartmentEventHandler>();
+	
+	private SendDepartmentAction action;
+	
+	public SendDepartmentEvent(SendDepartmentAction action, HasWidgets container)
+	{
+		super(action,container);
+	}
+	
+	public SendDepartmentAction getAction()
+	{
+		return action;
+	}
+	
+	@Override
+	public Type <SendDepartmentEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+	
+	@Override
+	protected void dispatch(SendDepartmentEventHandler handler)
+	{
+		handler.onSendDepartment(this);
+	}
+}
