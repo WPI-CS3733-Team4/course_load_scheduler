73068643bd897cfb7795a4d6c538d98f7ee13663
Robaire <robaire@galliath.com>
2018-02-26 00:01:39 -0500

    Updated for new UI

--- src/main/java/org/dselent/course_load_scheduler/client/CourseLoadScheduler.java
+++ src/main/java/org/dselent/course_load_scheduler/client/CourseLoadScheduler.java
@@ -1,16 +1,16 @@
 package org.dselent.course_load_scheduler.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import org.dselent.course_load_scheduler.client.gin.Injector;
+import org.dselent.course_load_scheduler.client.presenter.impl.IndexPresenterImpl;
 import org.dselent.course_load_scheduler.client.presenter.impl.*;
-import org.dselent.course_load_scheduler.client.service.impl.AdminServiceImpl;
-import org.dselent.course_load_scheduler.client.service.impl.BuilderServiceImpl;
-import org.dselent.course_load_scheduler.client.service.impl.FacultyServiceImpl;
-import org.dselent.course_load_scheduler.client.service.impl.UserServiceImpl;
+import org.dselent.course_load_scheduler.client.view.IndexView;
 import org.dselent.course_load_scheduler.client.view.*;
+
+import org.dselent.course_load_scheduler.client.widgets.AdminDataWidget;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -18,29 +18,88 @@ import org.dselent.course_load_scheduler.client.view.*;
 public class CourseLoadScheduler implements EntryPoint
 {
 
-    static public void registerScreen() {
-
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad()
+	{
 		RootLayoutPanel root = RootLayoutPanel.get();
 
+//        SimplePanelExample simplePanelExample = new SimplePanelExample();
+//        root.add(simplePanelExample);
+//
+//        SimplePanelExample2 simplePanelExample2 = new SimplePanelExample2();
+//        root.add(simplePanelExample2);
+//
+//
+//        HTMLPanelExample htmlPanelExample = new HTMLPanelExample();
+//        root.add(htmlPanelExample);
+//
+//
+//        FlowPanelExample flowPanelExample = new FlowPanelExample();
+//        root.add(flowPanelExample);
+//
+//        VerticalPanelExample verticalPanelExample = new VerticalPanelExample();
+//        root.add(verticalPanelExample);
+//
+//        HorizontalPanelExample horizontalPanelExample = new HorizontalPanelExample();
+//        root.add(horizontalPanelExample);
+//
+//        DockLayoutPanelExample dockLayoutPanelExample = new DockLayoutPanelExample();
+//        root.add(dockLayoutPanelExample);
+//
+//        GridExample gridExample = new GridExample();
+//        root.add(gridExample);
+//
+//        TabLayoutPanelExample tabLayoutPanelExample = new TabLayoutPanelExample();
+//        root.add(tabLayoutPanelExample);
+//
+//        ExamplesPanel examplesPanel = new ExamplesPanel();
+//        root.add(examplesPanel);
+//
+//		 Get the injector, which injected objects can be retrieved from
 		final Injector injector = Injector.INSTANCE;
-
-
-        IndexPresenterImpl indexPresenter = injector.getIndexPresenter(); // on-demand injection
+		
+		IndexPresenterImpl indexPresenter = injector.getIndexPresenter(); // on-demand injection
 		indexPresenter.init();
         IndexView indexView = indexPresenter.getView();
 
+		LoginPresenterImpl loginPresenter = injector.getLoginPresenter();
+		loginPresenter.init();
+        LoginView loginView = loginPresenter.getView();
+        
         RegisterPresenterImpl registerPresenter = injector.getRegisterPresenter();
         registerPresenter.init();
         RegisterView registerView = registerPresenter.getView();
 
-
-//        AdminServiceImpl asi = new AdminServiceImpl();
-//        asi.init();
+        FacultyPresenterImpl facultyPresenter = injector.getFacultyPresenter();
+        facultyPresenter.init();
+        FacultyView facultyView = facultyPresenter.getView();
+        
+        AdminPresenterImpl adminPresenter = injector.getAdminPresenter();
+        adminPresenter.init();
+        AdminView adminView = adminPresenter.getView();
+        
+        BuilderPresenterImpl builderPresenter = injector.getBuilderPresenter();
+        builderPresenter.init();
+        BuilderView builderView = builderPresenter.getView();
 
         indexPresenter.go(RootPanel.get("indexContainer"));
 		indexPresenter.go(root);
+		loginPresenter.go(indexView.getViewRootPanel());
+		//registerPresenter.go(indexView.getViewRootPanel());
 
-        registerPresenter.go(indexView.getViewRootPanel());
+		//facultyPresenter.go(indexView.getViewRootPanel());
+		//adminPresenter.go(indexView.getViewRootPanel());
+
+		//facultyPresenter.go(indexView.getViewRootPanel());
+		//adminPresenter.go(indexView.getViewRootPanel());
+ 
+		//builderPresenter.go(indexView.getViewRootPanel());
+		
+		
+		
+		
 	}
 	
 	static public void adminScreen() {
@@ -63,6 +122,11 @@ public class CourseLoadScheduler implements EntryPoint
 		indexPresenter.go(root);
 	
 		adminPresenter.go(indexView.getViewRootPanel());
+		
+		
+		// Populate the cell table
+		AdminDataWidget.populateUserTable(AdminDataWidget.testingUserTable());
+		AdminDataWidget.populateUserPermissionTable(AdminDataWidget.testingUserPermissionTable());
 	}
 	
 	static public void facultyScreen() {
@@ -108,81 +172,27 @@ public class CourseLoadScheduler implements EntryPoint
 	
 		builderPresenter.go(indexView.getViewRootPanel());
 	}
+	
+	static public void registerScreen() {
 
-    /**
-     * This is the entry point method.
-     */
-
-
-    public void onModuleLoad() {
 		RootLayoutPanel root = RootLayoutPanel.get();
 
 		final Injector injector = Injector.INSTANCE;
-
-        AdminServiceImpl adminService = injector.getAdminService();
-        adminService.init();
-        BuilderServiceImpl builderService = injector.getBuilderService();
-        builderService.init();
-        FacultyServiceImpl facultyService = injector.getFacultyService();
-        facultyService.init();
-        UserServiceImpl userService = injector.getUserService();
-        userService.init();
-
-
-
+		
 		IndexPresenterImpl indexPresenter = injector.getIndexPresenter(); // on-demand injection
 		indexPresenter.init();
         IndexView indexView = indexPresenter.getView();
 
-        LoginPresenterImpl loginPresenter = injector.getLoginPresenter();
-        loginPresenter.init();
-        LoginView loginView = loginPresenter.getView();
-
         RegisterPresenterImpl registerPresenter = injector.getRegisterPresenter();
         registerPresenter.init();
         RegisterView registerView = registerPresenter.getView();
-
-        FacultyPresenterImpl facultyPresenter = injector.getFacultyPresenter();
-        facultyPresenter.init();
-        FacultyView facultyView = facultyPresenter.getView();
-
-        AdminPresenterImpl adminPresenter = injector.getAdminPresenter();
-        adminPresenter.init();
-        AdminView adminView = adminPresenter.getView();
-
-        BuilderPresenterImpl builderPresenter = injector.getBuilderPresenter();
-        builderPresenter.init();
-        BuilderView builderView = builderPresenter.getView();
+        
+      
 
         indexPresenter.go(RootPanel.get("indexContainer"));
 		indexPresenter.go(root);
-        loginPresenter.go(indexView.getViewRootPanel());
-
-//        UserServiceImpl userService = injector.getUserService();
-//		userService.init();
-
-
-//		BuilderServiceImpl builderService = injector.getBuilderService();
-//		builderService.init();
-
-//		FacultyServiceImpl facultyService = injector.getFacultyService();
-//		facultyService.init();
-        //registerPresenter.go(indexView.getViewRootPanel());
-
-        //facultyPresenter.go(indexView.getViewRootPanel());
-        //adminPresenter.go(indexView.getViewRootPanel());
-
-        //facultyPresenter.go(indexView.getViewRootPanel());
-        //adminPresenter.go(indexView.getViewRootPanel());
-
-        //builderPresenter.go(indexView.getViewRootPanel());
-
-        GWT.log("helloWorld");
-
-
-
-
-
+	
+		registerPresenter.go(indexView.getViewRootPanel());
 	}
 	
 	static public void loginScreen() {
@@ -207,5 +217,3 @@ public class CourseLoadScheduler implements EntryPoint
 		loginPresenter.go(indexView.getViewRootPanel());
 	}
 }
-
-
